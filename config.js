cfce1b48a40e86820c98fb245a35327d195cfc57
Aaron Li-Mai <aaronmai@hotmail.com>
2021-05-14 22:17:46 -0600

    created

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  ownerID: "464635440801251328",
+  dashboard: {
+    callbackURL: "http://localhost:8080/callback",
+    domain: "localhost",
+    port: 8000,
+  },
+};
+
+module.exports = config;
